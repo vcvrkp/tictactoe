8c93c3159f9ffb170da8dd2526eb823b3e2137b9
vcvrkp <ramakrishnaprasad.vcv@gmail.com>
2020-12-01 15:53:26 +0530

    Changes for creation of invitation to other players

--- player/models.py
+++ player/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Invitation(models.Model):
+    from_user = models.ForeignKey(User, related_name="invitations_sent",on_delete=models.CASCADE)
+    to_user = models.ForeignKey(User, related_name="invitations_recieved",on_delete=models.CASCADE)
+    message = models.CharField(max_length=300)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return " {0} sent {1} an Invitation".format(self.from_user, self.to_user)
--- player/views.py
+++ player/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from gameplay.models import Game,GamesQuerySet
 from django.contrib.auth.decorators import login_required
+
+from .forms import InvitationForm
+from .forms import Invitation
 
 # Create your views here.
 @login_required
@@ -14,3 +17,16 @@ def home(request):
     dcontext['games'] = active
     return render(request=request, template_name="player/home.html",
                   context=dcontext)
+
+@login_required
+def new_invitation(request):
+    if request.method == "POST":
+        #TODO Handle the request of form submission.
+        invitation = Invitation(from_user=request.user)
+        form = InvitationForm(instance=invitation,data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('player_home')
+    else:
+        form = InvitationForm()
+    return render(request,"player/new_invitation_form.html",{"form":form})
